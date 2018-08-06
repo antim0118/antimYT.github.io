ea0897268fb947db6e5bab403d589bb45a5e01c3
antimYT <my.antim@inbox.ru>
2018-08-07 02:19:49 +0500

    Create ps.js

---
+++ ps.js
@@ -0,0 +1,10 @@
+function playStartGameSound() {
+        if (!gameFilledSnd) gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/lobby_notification_matchready.wav?raw=true");
+        gameFilledSnd.volume = .4;
+        gameFilledSnd.play();
+        gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/lobby_notification_matchready.wav?raw=true");
+        if (!gameFilledSnd) gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/game_ready_02.wav?raw=true");
+        gameFilledSnd.volume = .4;
+        gameFilledSnd.play();
+        gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/game_ready_02.wav?raw=true");
+}
