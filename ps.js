66dd1ee50197685208686cbcd5c1866018b84d36
antimYT <my.antim@inbox.ru>
2018-08-07 02:25:55 +0500

    Update ps.js

--- ps.js
+++ ps.js
@@ -1,10 +1,11 @@
+var gameFilledSnd;
 function playStartGameSound() {
-        if (!gameFilledSnd) gameFilledSnd = new Audio("https://raw.githubusercontent.com/antimYT/antimYT.github.io/master/lobby_notification_matchready.wav");
+        if (!gameFilledSnd) gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/lobby_notification_matchready.wav?raw=true");
         gameFilledSnd.volume = .4;
         gameFilledSnd.play();
-        gameFilledSnd = new Audio("https://raw.githubusercontent.com/antimYT/antimYT.github.io/master/lobby_notification_matchready.wav");
-        if (!gameFilledSnd) gameFilledSnd = new Audio("https://raw.githubusercontent.com/antimYT/antimYT.github.io/master/game_ready_02.wav");
+        gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/lobby_notification_matchready.wav?raw=true");
+        if (!gameFilledSnd) gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/game_ready_02.wav?raw=true");
         gameFilledSnd.volume = .4;
         gameFilledSnd.play();
-        gameFilledSnd = new Audio("https://raw.githubusercontent.com/antimYT/antimYT.github.io/master/game_ready_02.wav");
+        gameFilledSnd = new Audio("https://github.com/antimYT/antimYT.github.io/blob/master/game_ready_02.wav?raw=true");
 }
