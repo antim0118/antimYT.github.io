f001971513f99ae43d7504ce99c26f8e2c77815e
antimYT <my.antim@inbox.ru>
2018-08-08 19:04:05 +0500

    Update ps.js

--- ps.js
+++ ps.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PvPRO scripts
 // @namespace    https://github.com/antimYT/
-// @version      1.0
+// @version      1.1
 // @updateURL    https://raw.githubusercontent.com/antimYT/antimYT.github.io/master/ps.js
 // @description  PvPRO scripts
 // @author       me
